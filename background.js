1eb07c3679fd52394a10cbc15f6d15d6f5745491
Brian Lai <bhlai@andrew.cmu.edu>
2016-04-02 19:15:52 -0400

    withdrawal

--- background.js
+++ background.js
@@ -4,3 +4,19 @@
 function openTab(url) {
     chrome.tabs.create({ url: url})
 };
+
+function donate(apikey, accountID){
+  alert("git here");
+  require(['withdrawal'], function(withdrawal){
+    var withdrawalAccount = withdrawal.initWithKey(apikey);
+    var sampleWithdrawal = "{\"medium\": \"balance\",\"amount\": 10,\"description\": \"test\"}";  
+    console.log("[withdrawal - withdraw an account] Response: "+ withdrawalAccount.createWithdrawal(accountID, sampleWithdrawal));
+  });   
+}
+
+
+chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
+  console.log(message);
+  donate(message.key, message.acc);
+  sendResponse({stat : "complete"});
+});
