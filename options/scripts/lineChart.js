9a0af5e91ba44abebc4db5c2b4ad23cbf8d6890a
Alex Du <alex.du108@gmail.com>
2016-04-03 03:55:11 -0400

    wat

--- options/scripts/lineChart.js
+++ options/scripts/lineChart.js
@@ -1,27 +1,4 @@
-var pieData = [
-   {
-      value: 25,
-      label: 'Java',
-      color: '#811BD6'
-   },
-   {
-      value: 10,
-      label: 'Scala',
-      color: '#9CBABA'
-   },
-   {
-      value: 30,
-      label: 'PHP',
-      color: '#D18177'
-   },
-   {
-      value : 35,
-      label: 'HTML',
-      color: '#6AE128'
-   }
-];
-
-var lineData = {
+var data = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
     datasets: [
         {
@@ -33,9 +10,18 @@ var lineData = {
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
             data: [65, 59, 80, 81, 56, 55, 40]
+        },
+        {
+            label: "My Second dataset",
+            fillColor: "rgba(151,187,205,0.2)",
+            strokeColor: "rgba(151,187,205,1)",
+            pointColor: "rgba(151,187,205,1)",
+            pointStrokeColor: "#fff",
+            pointHighlightFill: "#fff",
+            pointHighlightStroke: "rgba(151,187,205,1)",
+            data: [28, 48, 40, 19, 86, 27, 90]
         }
     ]
 };
-
 var context = document.getElementById('skills').getContext('2d');
-var skillsChart = new Chart(context).Line(lineData);
+var skillsChart = new Chart(context).Line(data);
