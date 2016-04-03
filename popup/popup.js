4d964877285e91c59d93137b5bfc12c3bea198c4
Kevin Lee <kevin.hj.lee@gmail.com>
2016-04-03 00:05:27 -0400

    Extension popup menu now displays stats about donations over time by retreiving data from the database

--- popup/popup.js
+++ popup/popup.js
@@ -1,9 +1,46 @@
 var tab = null;
 
+let myAccountID = '5700296435b13979061e7069';
+let weekInMilli = 604800000;
+let dayInMilli = 86400000;
+
 function setInitialValues(apikey, custID, accID){
   require(['account'], function(account){
         var accounts =  account.initWithKey(apikey);
         $("#account_balance").text('$' + parseFloat(accounts.getAccountById(accID).balance).toFixed(2));
+
+        // Calculate your donation stats
+        $.get('https://burning-torch-5051.firebaseio.com/donations.json', function(success, error) {
+          if (success) {
+            console.log(success);
+
+            var donatedToday = 0;
+            var donatedThisWeek = 0;
+            var donatedToDate = 0;
+
+            for (var key in success) {
+              var donation = success[key];
+
+              if (donation.senderID === myAccountID) {
+                donatedToDate += donation.amount;
+
+                if (donation.time > (Date.now() - dayInMilli)) {
+                  donatedToday += donation.amount;
+                }
+
+                if (donation.time > (Date.now() - weekInMilli)) {
+                  donatedThisWeek += donation.amount;
+                }
+              }
+
+              $("#amount_donated_today").text('$' + donatedToday);
+              $("#amount_donated_this_week").text('$' + donatedThisWeek);
+              $("#amount_donated_to_date").text('$' + donatedToDate);
+            }
+          } else {
+            console.log("Firebase get request failed: " + error);
+          }
+        });
       });
 }
 
@@ -32,9 +69,6 @@ $(function() {
           window.close();
       }
   }
-
-
-  
 
   $("#account_menu_item").click(function() {
       BG.openTab("../options/index.html");
