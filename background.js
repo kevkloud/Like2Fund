413c4158c8f121643375561dc35a7c169a47918d
Kevin Lee <kevin.hj.lee@gmail.com>
2016-04-02 22:22:14 -0400

    firebase support

--- background.js
+++ background.js
@@ -1,3 +1,25 @@
+var database = new Firebase('https://burning-torch-5051.firebaseio.com/');
+
+$.get('https://burning-torch-5051.firebaseio.com/.json', function(success, error) {
+  if (success) {
+    console.log(success);
+  } else {
+    console.log(error);
+  }
+});
+
+database.set({name : "Kevin"});
+
+/*
+var data;
+
+database.on("value", function(snapshot) {
+  data = snapshot.val();
+  console.log(data);
+}, function (errorObject) {
+  console.log("The read failed: " + errorObject.code);
+});
+*/
 // Open a new tab with a given URL.
 // Inputs:
 //   url: string - url for the tab
@@ -5,18 +27,27 @@ function openTab(url) {
     chrome.tabs.create({ url: url})
 };
 
-function donate(apikey, accountID){
+function donate(apikey, senderID, receiverID, fbID) {
   alert("git here");
+
+  let amount = 10; 
+  //data.customerIDs[senderID].donationAmount;
+
   require(['withdrawal'], function(withdrawal){
     var withdrawalAccount = withdrawal.initWithKey(apikey);
-    var sampleWithdrawal = "{\"medium\": \"balance\",\"amount\": 10,\"description\": \"test\"}";  
-    console.log("[withdrawal - withdraw an account] Response: "+ withdrawalAccount.createWithdrawal(accountID, sampleWithdrawal));
-  });   
-}
+    var sampleWithdrawal = "{\"medium\": \"balance\",\"amount\": " + amount + ",\"description\": \"test\"}";  
+    console.log("[withdrawal - withdraw an account] Response: " + withdrawalAccount.createWithdrawal(senderID, sampleWithdrawal));
+  });
 
+  require(['deposit'], function(deposit) {
+    var depositAccount = deposit.initWithKey(apikey);
+    var sampleDeposit = "{\"medium\": \"balance\",\"status\": \"completed\", \"amount\": " + amount + ",\"description\": \"Dummy Deposit\"}";
+        console.log("[Deposit - New deposit]: " + depositAccount.createDeposit(depositAccount, sampleDeposit));
+  });
+}
 
 chrome.runtime.onMessage.addListener(function(message,sender,sendResponse){
   console.log(message);
-  donate(message.key, message.acc);
+  donate(message.key, message.sender, message.receiver, message.fbID);
   sendResponse({stat : "complete"});
 });
